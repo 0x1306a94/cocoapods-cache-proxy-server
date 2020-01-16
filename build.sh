94eaad2ff7f33280c8f94134e80560e88f2cf0ae
0x1306a94 <0x1306a94@gmail.com>
2020-01-16 16:46:06 +0800

    更新打包脚本

--- build.sh
+++ build.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 
-set -ex
+set -e
+
+#用法提示
+usage() {
+    echo "Usage:"
+    echo "  build.sh [-v --version]"
+    echo "Description:"
+    echo "    version 版本号."
+    exit -1
+}
+
+if [[ $# -lt 2 ]]; then
+    usage
+fi
+
+# 获取脚本执行时的选项
+while getopts v: option
+do
+   case "${option}"  in
+                v) Version=${OPTARG};;
+                h) usage;;
+                ?) usage;;
+   esac
+done
 
 COMMID=$(git rev-parse --short HEAD)
 NAME='cocoapods-cache-proxy-server'
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X main._version_=v1 -X main._commit_=$COMMID" -o ./bin/$NAME-linux-amd64
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-X main._version_=v1 -X main._commit_=$COMMID" -o ./bin/$NAME-darwin-amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X main._version_=$Version -X main._commit_=$COMMID" -o ./bin/$NAME-linux-amd64 && upx ./bin/$NAME-linux-amd64
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-X main._version_=$Version -X main._commit_=$COMMID" -o ./bin/$NAME-darwin-amd64 && upx ./bin/$NAME-darwin-amd64

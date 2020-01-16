cd293e97e337aac6ca842edf8fa2ef6b84e6d51b
0x1306a94 <0x1306a94@gmail.com>
2020-01-16 17:41:23 +0800

    添加docker配置

--- build.sh
+++ build.sh
@@ -27,5 +27,5 @@ done
 
 COMMID=$(git rev-parse --short HEAD)
 NAME='cocoapods-cache-proxy-server'
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X main._version_=$Version -X main._commit_=$COMMID" -o ./bin/$NAME-linux-amd64 && upx ./bin/$NAME-linux-amd64
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-X main._version_=$Version -X main._commit_=$COMMID" -o ./bin/$NAME-darwin-amd64 && upx ./bin/$NAME-darwin-amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X main._version_=$Version -X main._commit_=$COMMID" -o ./docker/bin/$NAME-linux-amd64 && upx ./docker/bin/$NAME-linux-amd64
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-X main._version_=$Version -X main._commit_=$COMMID" -o ./docker/bin/$NAME-darwin-amd64 && upx ./docker/bin/$NAME-darwin-amd64
